3e2d5a16169e298c987099aec4d636ae159bafcd
lisawium <wiumlisa@gmail.com>
2026-03-10 23:56:16 +0100

    Added first implementation of Building, Events

--- src/main/java/it/polimi/ingsw/CavePaintings.java
+++ src/main/java/it/polimi/ingsw/CavePaintings.java
@@ -1,4 +1,26 @@
 package it.polimi.ingsw;
 
 public class CavePaintings extends Event {
+    public String NAME = "CavePaintings";
+    // se si hanno un numero di carte artista pari a quelli segnati sopra si ottengono i punti sopra,
+    // altimenti se si hanno quelli sotto si tolgono i punti sotto
+    public int topNumArtist;
+    public int topPp;
+    public int bottomNumArtist;
+    public int bottomPp;
+    // per essere usata la carta CavePaintings il giocatore deve avere in mano minimo tot Artisti
+    public int minArtist;
+
+    @Override
+    public Era getEra(){return null;}
+
+    @Override
+    public String getName(){
+        return NAME;
+    }
+
+    @Override
+    public void applyEffect(Player p){
+
+    }
 }
--- src/main/java/it/polimi/ingsw/ShamanicRitual.java
+++ src/main/java/it/polimi/ingsw/ShamanicRitual.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw;
 
 public class ShamanicRitual extends Event {
+    public String NAME = "ShamanicRitual";
+
+    @Override
+    public Era getEra(){return null;}
+
+    @Override
+    public String getName(){
+        return NAME;
+    }
+    @Override
+    public void applyEffect(Player p){
+
+    }
 }
