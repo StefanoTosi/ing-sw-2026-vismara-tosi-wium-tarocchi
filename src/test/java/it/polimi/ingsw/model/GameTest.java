5fbcaf85ef3ba9cb0708a71e687d5160c8c6d828
lisawium <wiumlisa@gmail.com>
2026-03-24 14:46:01 +0100

    Fixed PlayerTests and added GameTests

--- src/test/java/it/polimi/ingsw/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.characters.Artist;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,25 +24,67 @@ class GameTest {
 
     @Test
     void getPlayers() throws IllegalArgumentException{
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
+        List<Player> test = Arrays.asList(player1, player2, player3);
+        assert game.getPlayers().equals(test);
     }
 
     @Test
     void getPlayerPositions() throws IllegalArgumentException{
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
+        game.putPlayerPosition(player1, 'A');
+        game.putPlayerPosition(player2, 'B');
+        game.putPlayerPosition(player3, 'C');
     }
 
     @Test
     void putPlayerPosition() throws IllegalArgumentException {
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
+        game.putPlayerPosition(player1, 'A');
+        game.putPlayerPosition(player2, 'B');
+        game.putPlayerPosition(player3, 'C');
     }
 
     @Test
     void getBoard() throws IllegalArgumentException{
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
+        Board board = new Board(3);
+        Card card = new Artist(Era.II);
+        Card card2 = new Artist(Era.II);
+        Card card3 = new Artist(Era.II);
+        ArrayList<Card> topRowTribe = new ArrayList<Card>(Arrays.asList(card, card2, card3));
+        board.setTopRowTribe(topRowTribe);
+        game.getBoard().setTopRowTribe(topRowTribe);
+        assertEquals(board.getTopRowTribe().size(), game.getBoard().getTopRowTribe().size());
     }
 
     @Test
     void getState() throws IllegalArgumentException {
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
     }
 
     @Test
     void setState()throws IllegalArgumentException {
+        Player player1 = new Player("Elisa");
+        Player player2 = new Player("Stefano");
+        Player player3 = new Player("Gilles");
+        Game game = new Game(new ArrayList<Player>(Arrays.asList(player1, player2, player3)));
+
+
     }
 }
