6cc655536fca99be6c65dcbfedd5ea8ede5ebe1d
Gilles <gvsgames.mail@gmail.com>
2026-03-28 17:14:35 +0100

    Tested characters and adde JavaDoc to Effect
    (cherry picked from commit cf20679585d00e9c3d715e93f1f3f5331157281c)
    
    # Conflicts:
    #	src/main/java/it/polimi/ingsw/model/Player.java

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -240,7 +240,7 @@ public class Player {
         int count = 0;
 
         for (Shaman shaman : shamans) {
-            count += shaman.getStars();
+                count += shaman.getStars();
         }
         return count;
     }
@@ -252,7 +252,7 @@ public class Player {
     public int countBuildersPp() {
         int points = 0;
         for (Builder builder : builders) {
-            points += builder.getPp();
+                points += builder.getPp();
         }
         return points;
     }
@@ -272,20 +272,20 @@ public class Player {
         points += this.countBuildersPp();
 
         for (Inventor inventor : inventors) {
-            found = false;
-            currIcon = inventor.getInventionIcon();
+                found = false;
+                currIcon = inventor.getInventionIcon();
 
-            for(Icon icon : seenIcons){
-                if (currIcon == icon){
-                    found = true;
+                for(Icon icon : seenIcons){
+                    if (currIcon == icon){
+                        found = true;
+                    }
+                }
+
+                if(!found){
+                    seenIcons.add(currIcon);
+                    numIcons++;
                 }
             }
-
-            if(!found){
-                seenIcons.add(currIcon);
-                numIcons++;
-            }
-        }
 
         points += numIcons * this.getNumInventors();
 
