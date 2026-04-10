262c78b75620b0e8748b54716de2f88fd679a2d7
lisawium <wiumlisa@gmail.com>
2026-04-10 15:26:40 +0200

    Print board with cards displayed orizzontally

--- src/main/java/it/polimi/ingsw/networking/TUI.java
+++ src/main/java/it/polimi/ingsw/networking/TUI.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.networking;
 import it.polimi.ingsw.model.Card;
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.characters.Artist;
+import it.polimi.ingsw.model.characters.Hunter;
+import it.polimi.ingsw.model.characters.Inventor;
 import it.polimi.ingsw.model.effects.Building;
 import it.polimi.ingsw.model.exceptions.IllegalActionException;
 
@@ -70,46 +74,131 @@ public class TUI implements UIObserver {
         tui.start();
     }
 
-
-    public void printBoard(Board board){
-        printRowTribe(board.getTopRowTribe());
-        printRowTribe(board.getBottomRowTribe());
-        printRowBuilding(board.getTopRowBuilding());
-        printRowBuilding(board.getBottomRowBuilding());
-    }
-    public String[] type = {"Event", "Chara", "Build"};
-    public void printRowTribe(List<Card> cards){
-        for(Card card : cards) {
-            System.out.println("+-------+" +
-                    "\n|" + Character.getName() + "|" +
-                    "\n|  " + card.getEra() + "  |" +
-                    "\n| idk   |" +
-                    "\n+-------+");
-        }
-    }
-
-    public void printRowBuilding(List<Building> buildings){
-        for(Building building : buildings) {
-            System.out.println("+-------+" +
-                    "\n| Build |" +
-                    "\n|  " + card.getEra() + "  |" +
-                    "\n|effetto|" +
-                    "\n+-------+");
-        }
-    }
-
-}
-public void printEvent(){
-    System.out.println("________" +
-            "\n| type   |" +
-            "\n| pp     |" +
-            "\n| idk    |" +
-            "\n|________|");
-}
-
     @Override
     public void update() throws RemoteException, IllegalActionException {
         System.out.println("Ricevuto aggiornamento");
         //this.game = controller.getGame(client.getNickname());
     }
 }
+
+public void printBoard(Board board){
+    printRowTribe(board.getTopRowTribe());
+    printRowTribe(board.getBottomRowTribe());
+    printRowBuilding(board.getTopRowBuilding());
+    printRowBuilding(board.getBottomRowBuilding());
+}
+
+public void printRowTribe(List<Card> cards){
+
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for(Card card : cards) {
+        line1.append("+-------+");
+        line2.append("|" + card.getName() + "|");
+        line3.append("|" + card.getEra() + "|");
+        line4.append("| idk   |");
+        line5.append("+-------+");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
+
+public void printRowBuilding(List<Building> buildings){
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for(Building building : buildings) {
+        line1.append("+-------+");
+        line2.append("| Build |");
+        line3.append("|" + building.getCost() + "|");
+        line4.append("| " + building.getEffect() + " |");
+        line5.append("+-------+");
+    }
+}
+
+public void printPlayerCards(List<Player> players){
+    for(Player player : players){
+        System.out.println("\n=== " + player.getName() + " ===");
+        printRowBuilding(player.getBuildings());
+        printArtist(player.getArtists());
+        //printGatherer(player.getGatherers());
+        printHunter(player.getHunters());
+        printInventor(player.getInventors());
+    }
+}
+
+public void printArtist(List<Artist> artists) {
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for (Artist artist : artists) {
+        line1.append("+-------+");
+        line2.append("|Artist |");
+        line3.append("|" + artist.getEra() + "|");
+        line4.append("|       |");
+        line5.append("+-------+");
+    }
+}
+
+public void printGatherer(List<Gatherer> gatherers) {
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for (Gatherer gatherer : gatherers) {
+        line1.append("+-------+");
+        line2.append("|Gather |");
+        line3.append("|       |");
+        line4.append("|       |");
+        line5.append("+-------+");
+    }
+}
+
+public void printHunter(List<Hunter> hunters) {
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for (Hunter hunter : hunters) {
+        line1.append("+-------+");
+        line2.append("|Hunter |");
+        line3.append("| " + hunter.getEra() + " |");
+        line4.append("| " + hunter.getIcon() + " |");
+        line5.append("+-------+");
+    }
+}
+
+public void printInventor(List<Inventor> inventors) {
+    StringBuilder line1 = new StringBuilder();
+    StringBuilder line2 = new StringBuilder();
+    StringBuilder line3 = new StringBuilder();
+    StringBuilder line4 = new StringBuilder();
+    StringBuilder line5 = new StringBuilder();
+
+    for (Inventor inventor : inventors) {
+        line1.append("+-------+");
+        line2.append("|Invent |");
+        line3.append("|" + inventor.getEra() + "|");
+        line4.append("|" + inventor.getInventionIcon() + "|");
+        line5.append("+-------+");
+    }
+}
+
