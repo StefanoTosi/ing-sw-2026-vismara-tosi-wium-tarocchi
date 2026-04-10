a9728f0620468cba84c0e6548c281d40672bbd8c
lisawium <wiumlisa@gmail.com>
2026-04-10 12:06:19 +0200

    Try changes for board printing

--- src/main/java/it/polimi/ingsw/networking/TUI.java
+++ src/main/java/it/polimi/ingsw/networking/TUI.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.networking;
+import it.polimi.ingsw.model.Card;
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.effects.Building;
+
 import java.rmi.RemoteException;
 import java.util.Scanner;
 
@@ -52,5 +56,41 @@ public class TUI {
             default:
                 System.out.println("Invalid answer");
         }
+    }
+
+    public void printBoard(Board board){
+        printRowTribe(board.getTopRowTribe());
+        printRowTribe(board.getBottomRowTribe());
+        printRowBuilding(board.getTopRowBuilding());
+        printRowBuilding(board.getBottomRowBuilding());
+    }
+    public String[] type = {"Event", "Chara", "Build"};
+    public void printRowTribe(List<Card> cards){
+        for(Card card : cards) {
+            System.out.println("+-------+" +
+                    "\n|" + Character.getName() + "|" +
+                    "\n|  " + card.getEra() + "  |" +
+                    "\n| idk   |" +
+                    "\n+-------+");
+        }
+    }
+
+    public void printRowBuilding(List<Card> cards){
+        for(Card card : cards) {
+            System.out.println("+-------+" +
+                    "\n| Build |" +
+                    "\n|  " + card.getEra() + "  |" +
+                    "\n|effetto|" +
+                    "\n+-------+");
+        }
+    }
+
+    }
+    public void printEvent(){
+        System.out.println("________" +
+                "\n| type   |" +
+                "\n| pp     |" +
+                "\n| idk    |" +
+                "\n|________|");
     }
 }
