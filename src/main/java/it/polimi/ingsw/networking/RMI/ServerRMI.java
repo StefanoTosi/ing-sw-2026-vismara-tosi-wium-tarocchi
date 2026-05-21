3c17f2e6614cb25c90a4ed7ae9559e495bc5ded3
Stefano Tosi <stefano.tosi.st04@gmail.com>
2026-05-21 14:57:08 +0200

    Fixed error on loading game without DB

--- src/main/java/it/polimi/ingsw/networking/RMI/ServerRMI.java
+++ src/main/java/it/polimi/ingsw/networking/RMI/ServerRMI.java
@@ -39,7 +39,7 @@ public class ServerRMI extends UnicastRemoteObject implements Controller {
         synchronized (lock) {
             if (users.containsKey(nickname)) {
                 user = users.get(nickname);
-                if(user.getPassword() == null) {
+                if(user.getPassword().equals("")) {
                     user.setPassword(psw);
                 }
                 if (user.getPassword().equals(psw)) {
